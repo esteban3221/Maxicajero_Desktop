e88a397a3ed8cf9c7fec4143ac3e5e4c0d50af0b
esteban3221 <esteban-oe100@hotmail.com>
2024-08-19 19:15:15 -0600

    cambios en vista de pago

--- src/view/menu/pago.cpp
+++ src/view/menu/pago.cpp
@@ -3,9 +3,11 @@
 Pago::Pago(/* args */)
 {
     this->set_orientation(Gtk::Orientation::VERTICAL);
-    
+    this->set_spacing(5);
+    tresfilas.set_halign(Gtk::Align::CENTER);
     this->append(tresfilas);
     this->append(nip);
+    nip.entry->set_editable(false);
 }
 
 Pago::~Pago()
