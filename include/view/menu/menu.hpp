75ba269ff0fb078fc7b36cacadfc087fe9cd351d
esteban3221 <esteban-oe100@hotmail.com>
2024-08-21 14:49:54 -0600

    yoshi

--- include/view/menu/menu.hpp
+++ include/view/menu/menu.hpp
@@ -2,11 +2,8 @@
 #include <gtkmm.h>
 #include "global.hpp"
 #include "controller/menu/pago.hpp"
-<<<<<<< HEAD
 #include "controller/menu/consultaEfectivo.hpp"
-=======
 #include "controller/menu/venta.hpp"
->>>>>>> 67c5b51daec8c5b4a577e0e3774a39943dd11ee3
 
 class ViewMenu : public Gtk::Box
 {
@@ -17,12 +14,9 @@ private:
 
     //=========== Vistas ==========
     ControllerPago pago;
-<<<<<<< HEAD
     ControllerConsultaEfectivo efectivo;
-=======
     ControllerVenta venta;
     
->>>>>>> 67c5b51daec8c5b4a577e0e3774a39943dd11ee3
 protected:
     Gtk::Stack *stackMenu = nullptr;
 
